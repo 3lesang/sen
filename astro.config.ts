7c8f39b8c603d11c82549756445b6a207d4e476e
Sang Le <3lesang@gmail.com>
2025-07-06 20:44:13 +0700

    build(ssr): update

--- astro.config.ts
+++ astro.config.ts
@@ -1,27 +1,42 @@
 // @ts-check
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
   build: {
     format: "file",
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react(), AstroPWA(), sitemap()],
+
   prefetch: {
     defaultStrategy: "hover",
     prefetchAll: true,
   },
+
   trailingSlash: "never",
+
+  i18n: {
+    locales: ["en", "vi"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+
   server: {
     port: 3000,
   },
+
+  adapter: netlify(),
 });
